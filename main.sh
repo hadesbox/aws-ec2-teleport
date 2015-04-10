53a284c5c72710e6cb8ee5da307ddca3a3c611b2
Luis Gonzalez <hadesbox@gmail.com>
2015-04-10 12:08:15 +0200

    adding some

--- main.sh
+++ main.sh
@@ -2,9 +2,9 @@
 
 export NODE_PATH=/usr/local/lib/node_modules/teleport/node_modules
 
-node /usr/local/lib/node_modules/teleport/main.js $1 $2 > /tmp/salida
-
 rm /tmp/salida 2>/dev/null
+
+node /usr/local/lib/node_modules/teleport/main.js $1 $2 > /tmp/salida
 
 if [ $? -eq 0 ]; then
    ssh -i $(cat /tmp/salida)
