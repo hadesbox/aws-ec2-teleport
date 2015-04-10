89870a6e9a24d246f9c771de9d8e370bd516e9fc
Luis Gonzalez <hadesbox@gmail.com>
2015-04-10 09:30:15 +0200

    Adding fix for teleport

--- main.sh
+++ main.sh
@@ -4,11 +4,13 @@ export NODE_PATH=/usr/local/lib/node_modules/teleport/node_modules
 
 node /usr/local/lib/node_modules/teleport/main.js $1 $2 > /tmp/salida
 
+rm /tmp/salida 2>/dev/null
+
 if [ $? -eq 0 ]; then
-   ssh -i $(cat salida)
+   ssh -i $(cat /tmp/salida)
    exit;
 else
-   cat /tmp/salida
-   rm /tmp/salida
+   cat /tmp/salida 2>/dev/null
+   rm /tmp/salida 2>/dev/null
    exit;
 fi
